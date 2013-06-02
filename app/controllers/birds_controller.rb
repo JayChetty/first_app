0f2d9d7bad3f8bb6b0000b01bf26798269d6c843
Jay <jay@ubuntu.(none)>
2013-06-02 17:07:35 +0100

    Initial Commit

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,5 @@
+class BirdsController < ApplicationController
+	def call	
+		@call = 'tweet tweet'
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = 'db4e46c6c7535c3875e24afca5bca4a27702895fa9df0b39668875cdf0b5c86f475a3d4f433c64d5a8f037d14a6ec631cc6730b27c1c1b131f706538c2a37c75'
